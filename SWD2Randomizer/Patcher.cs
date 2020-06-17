a1056f7e92e14e9da0720793a0c4f97c5700a6f8
Clément Gallet <clement.gallet@ens-lyon.org>
2020-06-17 18:30:50 +0200

    Fix quests

--- SWD2Randomizer/Patcher.cs
+++ SWD2Randomizer/Patcher.cs
@@ -19,30 +19,7 @@ namespace SWD2Randomizer
             PatchIntro();
             PatchPriest();
             PatchHook();
-/*            PatchElMachino();
-*/        }
-
-        /* Remove intro cutscene */
-        public void PatchIntro()
-        {
-            string introPatch = Path.Combine(baseDir, "Patchsets", "WestDesert", "west_desert_intro.le");
-
-            XmlDocument doc = new XmlDocument();
-            try
-            {
-                doc.Load(introPatch);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("caught exception");
-            }
-
-            XmlNode cutsceneScript = doc.SelectSingleNode("//ScriptEntity[Id='32564401']");
-
-            if (cutsceneScript != null)
-                cutsceneScript.ParentNode.RemoveChild(cutsceneScript);
-
-            doc.Save(introPatch);
+            PatchTriple();
         }
 
         /* Make the ignition axe effective against Priest Glorious */
@@ -114,11 +91,40 @@ namespace SWD2Randomizer
             doc.Save(priceList);
         }
 
-        public void PatchElMachino()
+        /* Remove intro cutscene */
+        public void PatchIntro()
         {
-            string introPatch = Path.Combine(baseDir, "Patchsets", "WestDesert", "west_desert_intro.le");
+            string quests = Path.Combine(baseDir, "Definitions", "quests.xml");
 
             XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.Load(quests);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("caught exception");
+            }
+
+            XmlNode questEarthquake = doc.SelectSingleNode("//Quest[@Name='quest_earthquake']");
+            questEarthquake.Attributes["Template"].Value = "SIDE_QUEST";
+
+            XmlNode questTemple = doc.SelectSingleNode("//Quest[@Name='quest_enter_temple']");
+            questTemple.RemoveChild(questTemple["UnlockedBy"]);
+
+            XmlNode questFen = doc.SelectSingleNode("//Quest[@Name='quest_find_fen']");
+            questFen.Attributes["Template"].Value = "SIDE_QUEST";
+            questFen.RemoveChild(questFen["UnlockedBy"]);
+
+            XmlNode questRelFen = doc.SelectSingleNode("//Quest[@Name='quest_release_fen']");
+            questRelFen.Attributes["Template"].Value = "SIDE_QUEST";
+
+            XmlNode questExitTemple = doc.SelectSingleNode("//Quest[@Name='quest_exit_temple']");
+            questExitTemple["UnlockedBy"].InnerText = "quest_get_run_boots";
+
+            doc.Save(quests);
+
+            string introPatch = Path.Combine(baseDir, "Patchsets", "WestDesert", "west_desert_intro.le");
             try
             {
                 doc.Load(introPatch);
@@ -128,59 +134,86 @@ namespace SWD2Randomizer
                 Console.WriteLine("caught exception");
             }
 
-/*            XmlNode elmachinoReload = doc.SelectSingleNode("//ScriptEntity[Definition='ProgressQuest']/Property[Name='QuestName' and Value='quest_tutorial_indicators']");
-            XmlNode target = doc.SelectSingleNode("//Connection[TargetId='1042288']");
+            XmlNode checkQuestProp = doc.SelectSingleNode("//ScriptEntity[Id='32564402']");
 
-            target["TargetId"].InnerText = "32578602";*/
+            checkQuestProp.InnerXml = @"
+			<Id>32564402</Id>
+			<Name>SetUpgrade2</Name>
+			<Position>1664, 2035</Position>
+			<Definition>SetUpgrade</Definition>
+			<Area>1562, 2003, 203, 63</Area>
+			<Connections>
+				<Connection>
+					<SourceContact>out</SourceContact>
+					<TargetContact>in</TargetContact>
+					<TargetId>32564403</TargetId>
+				</Connection>
+			</Connections>
+            <Property>
+                <Name>UpgradeId</Name>
+                <Type>String</Type>
+                <Value>minimap</Value>
+            </Property>
+            <Property>
+                <Name>Tier</Name>
+                <Type>Int32</Type>
+                <Value>1</Value>
+            </Property>
+            <Property>
+                <Name>HideNewMarker</Name>
+                <Type>Boolean</Type>
+                <Value>True</Value>
+            </Property>
+            ";
 
+            checkQuestProp = doc.SelectSingleNode("//ScriptEntity[Id='32564403']");
 
-            XmlNode newScript = doc.CreateNode("element", "ScriptEntity", "");
-            newScript.InnerXml = @"
-				<Id>92578602</Id>
-				<Name>ProgressQuest</Name>
-              <Position>3291, 3111</Position>
-              <Definition>ProgressQuest</Definition>
-              <Area>3203, 3079, 174, 63</Area>
-              <Connections />
-				<Property>
-					<Name>QuestName</Name>
-					<Type>String</Type>
-					<Value>quest_lit_the_lamp</Value>
-				</Property>
-				<Property>
-					<Name>ObjectiveName</Name>
-					<Type>String</Type>
-					<Value>obj_lit_the_lamp</Value>
-				</Property>
-				<Property>
-					<Name>Increase</Name>
-					<Type>Int32</Type>
-					<Value>1</Value>
-				</Property>";
+            checkQuestProp.InnerXml = @"
+			<Id>32564403</Id>
+			<Name>ProgressQuest4</Name>
+			<Position>1839, 2035</Position>
+			<Definition>ProgressQuest</Definition>
+			<Area>1807, 2003, 63, 63</Area>
+			<Connections>
+				<Connection>
+					<SourceContact>out</SourceContact>
+					<TargetContact>in</TargetContact>
+					<TargetId>32564404</TargetId>
+				</Connection>
+			</Connections>
+			<Property>
+				<Name>QuestName</Name>
+				<Type>String</Type>
+				<Value>quest_lit_the_lamp</Value>
+			</Property>
+			<Property>
+				<Name>ObjectiveName</Name>
+				<Type>String</Type>
+				<Value>obj_lit_the_lamp</Value>
+			</Property>
+			<Property>
+				<Name>Increase</Name>
+				<Type>Int32</Type>
+				<Value>1</Value>
+			</Property>
+            ";
 
-            XmlNode newEnter = doc.CreateNode("element", "ScriptEntity", "");
-            newEnter.InnerXml = @"							<Id>32564401</Id>
-							<Name>OnEnter9</Name>
-              <Position>2391, 3348</Position>
-							<Definition>OnEnter</Definition>
-              <Area>2322, 3316, 97, 63</Area>
-							<Connections>
-								<Connection>
-									<SourceContact>out</SourceContact>
-									<TargetContact>in</TargetContact>
-									<TargetId>92578602</TargetId>
-								</Connection>
-							</Connections>
-							<Property>
-								<Name>TriggerOnlyOnce</Name>
-								<Type>Boolean</Type>
-								<Value>True</Value>
-							</Property>";
+            checkQuestProp = doc.SelectSingleNode("//ScriptEntity[Id='32564404']");
 
-            XmlNode layer = doc.SelectSingleNode("//TileLayer[Id='1040543']");
+            checkQuestProp.InnerXml = @"
+			<Id>32564404</Id>
+			<Name>CompleteQuest</Name>
+			<Position>1988, 2035</Position>
+			<Definition>CompleteQuest</Definition>
+			<Area>1921, 2003, 132, 63</Area>
+			<Connections />
+			<Property>
+				<Name>QuestName</Name>
+				<Type>String</Type>
+				<Value>quest_arrowtrap_defeated</Value>
+			</Property>
+            ";
 
-            layer["Entities"].AppendChild(newScript);
-            layer["Entities"].AppendChild(newEnter);
 
             doc.Save(introPatch);
         }
